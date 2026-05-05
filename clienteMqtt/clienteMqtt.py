77c13672d01943d4d3fd42197571551817c856f8
Hermann Carlos <hermannalexander8@gmail.com>
2026-05-05 16:28:29 -0300

    ejercicio docker

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -1,22 +1,54 @@
 import asyncio, ssl, certifi, logging, os
 import aiomqtt
 
-logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
+logging.basicConfig(
+    format='%(asctime)s - - %(levelname)s:%(message)s',
+    level=logging.INFO,
+    datefmt='%d/%m/%Y %H:%M:%S %z'
+)
+
+async def atender_topico_1(contenido):
+    logging.info(f"Tópico 1: {contenido}")
+
+async def atender_topico_2(contenido):
+    logging.info(f"Tópico 2: {contenido}")
 
 async def main():
+    # Obtener variables de entorno
+    servidor = os.environ.get('SERVIDOR')
+    topico_1 = os.environ.get('TOPICO_1')
+    topico_2 = os.environ.get('TOPICO_2')
+    topico_publicar = os.environ.get('TOPICO_PUBLICAR')
+
+    # Configuración MQTTS
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     tls_context.verify_mode = ssl.CERT_REQUIRED
     tls_context.check_hostname = True
     tls_context.load_default_certs()
 
-    async with aiomqtt.Client(
-        os.environ['SERVIDOR'],
-        port=8883,
-        tls_context=tls_context,
-    ) as client:
-        await client.subscribe(os.environ['TOPICO'])
+    #Unico cliente MQTT
+    async with aiomqtt.Client(servidor, port=8883, tls_context=tls_context) as client:
+
+        # Suscribirse a los tópicos
+        await client.subscribe(topico_1)
+        await client.subscribe(topico_2)
+
         async for message in client.messages:
-            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+            contenido = message.payload.decode("utf-8") 
+
+            if message.topic == topico_1:
+                asyncio.create_task(
+                    atender_topico_1(contenido), 
+                    name="Tarea-Suscripcion-1"
+                )
+            elif message.topic == topico_2:
+                asyncio.create_task(
+                    atender_topico_2(contenido), 
+                    name="Tarea-Suscripcion-2"
+                )
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logging.info("Programa detenido por el usuario")
