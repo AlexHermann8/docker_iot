ee789084132c1ef00f4d5953b1f20740c3e95dd2
Hermann Carlos <hermannalexander8@gmail.com>
2026-06-02 21:11:58 -0300

    eliminacion de funciones qeu no se utilizan para el ejercicio

--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -46,8 +46,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         apellido=""
     kb = [
-        ["temperatura","humedad"],
-        ["gráfico temperatura","gráfico humedad"],
         ["modo auto","modo manual"],
         ["rele ON","rele OFF"],
         ["destello"]
@@ -69,63 +67,6 @@ async def baile(update: Update, context):
         await context.bot.send_message(update.message.chat.id, text="¡Patito feliz!")
     else:
         await context.bot.send_message(update.message.chat.id, text="¡No puede bailar!")
-        
-async def medicion(update: Update, context):
-    logging.info(update.message.text)
-    sql = f"SELECT timestamp, {update.message.text} FROM mediciones ORDER BY timestamp DESC LIMIT 1"
-    conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
-                                    user=os.environ["MARIADB_USER"],
-                                    password=os.environ["MARIADB_USER_PASS"],
-                                    db=os.environ["MARIADB_DB"])
-    async with conn.cursor() as cur:
-        await cur.execute(sql)
-        r = await cur.fetchone()
-        if update.message.text == 'temperatura':
-            unidad = 'ºC'
-        else:
-            unidad = '%'
-        await context.bot.send_message(update.message.chat.id,
-                                    text="La última {} es de {} {},\nregistrada a las {:%H:%M:%S %d/%m/%Y}"
-                                    .format(update.message.text, str(r[1]).replace('.',','), unidad, r[0]))
-        logging.info("La última {} es de {} {}, medida a las {:%H:%M:%S %d/%m/%Y}".format(update.message.text, r[1], unidad, r[0]))
-    conn.close()
-
-async def graficos(update: Update, context):
-    logging.info(update.message.text)
-    sql = f"""SELECT timestamp, {update.message.text.split()[1]}
-            FROM (
-                SELECT timestamp, {update.message.text.split()[1]},
-                    ROW_NUMBER() OVER (ORDER BY id) AS rn
-                FROM mediciones
-                WHERE timestamp >= NOW() - INTERVAL 1 DAY
-                AND sensor_id LIKE 'sensor_1'
-            ) AS t
-            WHERE rn % 2 = 0
-            ORDER BY timestamp;"""
-    conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
-                                    user=os.environ["MARIADB_USER"],
-                                    password=os.environ["MARIADB_USER_PASS"],
-                                    db=os.environ["MARIADB_DB"])
-    async with conn.cursor() as cur:
-        await cur.execute(sql)
-        filas = await cur.fetchall()
-
-        fig, ax = plt.subplots(figsize=(7, 4))
-        fecha,var=zip(*filas)
-        ax.plot(fecha,var)
-        ax.grid(True, which='both')
-        ax.set_title(update.message.text, fontsize=14, verticalalignment='bottom')
-        ax.set_xlabel('fecha')
-        ax.set_ylabel('unidad')
-
-        buffer = BytesIO()
-        fig.tight_layout()
-        fig.savefig(buffer, format='png')
-        plt.close()
-        buffer.seek(0)
-        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=buffer)
-        buffer.close()
-    conn.close()
 
 async def setpoint(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not context.args:
@@ -181,8 +122,6 @@ def main():
     application.add_handler(CommandHandler('baile', baile))
     application.add_handler(CommandHandler('setpoint', setpoint))
 
-    application.add_handler(MessageHandler(filters.Regex("^(temperatura|humedad)$"), medicion))
-    application.add_handler(MessageHandler(filters.Regex("^(gráfico temperatura|gráfico humedad)$"), graficos))
     application.add_handler(MessageHandler(filters.Regex("^(modo auto|modo manual)$"), modo))
     application.add_handler(MessageHandler(filters.Regex("^(rele ON|rele OFF)$"), rele))
     application.add_handler(MessageHandler(filters.Regex("^(destello)$"), destello))
